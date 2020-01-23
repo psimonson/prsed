ff9881f1294f79a1a6e087c76c735c94ede92b1e
Philip Simonson <psimonson1988@gmail.com>
2020-01-23 13:31:51 -0500

    Getting closer to an editor

--- src/main.c
+++ src/main.c
@@ -3,32 +3,94 @@
 #include <termios.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <errno.h>
 
-static struct termios orig_term;
-
+/* Control+k macro */
+#define CTRL_KEY(k) ((k) & 0x1f)
+/* Editor config structure */
+struct editor_config {
+	struct termios orig_termios;
+};
+/* Editor config definition */
+struct editor_config e;
+/* Exit out of the program and report an error.
+ */
+void die(const char *msg)
+{
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+	perror(msg);
+	exit(1);
+}
 /* Disable raw mode.
  */
 static void disable_raw()
 {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_term);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &e.orig_termios) < 0)
+		die("tcsetattr");
 }
 /* Enable raw mode.
  */
 void enable_raw()
 {
 	struct termios raw;
-	tcgetattr(STDIN_FILENO, &orig_term);
+	if(tcgetattr(STDIN_FILENO, &e.orig_termios) < 0)
+		die("tcgetattr");
 	atexit(disable_raw);
 
 	/* setup raw mode */
-	raw = orig_term;
+	raw = e.orig_termios;
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) < 0)
+		die("tcsetattr");
+}
+/* Draw rows for editor.
+ */
+void editor_draw_rows()
+{
+	int y;
+	for(y = 0; y < 24; y++)
+		write(STDOUT_FILENO, "~\r\n", 3);
+}
+/* Clear the screen.
+ */
+void editor_refresh_screen()
+{
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+	editor_draw_rows();
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+/* Read input from user.
+ */
+static char editor_read_key()
+{
+	int nread;
+	char c;
+	while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if(nread < 0 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
+/* Process key presses from user.
+ */
+void editor_process_key() {
+	char c = editor_read_key();
+
+	switch(c) {
+	case CTRL_KEY('q'):
+		write(STDOUT_FILENO, "\x1b[2J", 4);
+		write(STDOUT_FILENO, "\x1b[H", 3);
+		exit(0);
+	break;
+	default:
+	break;
+	}
 }
 /* Simple Text Editor (prsed).
  */
@@ -36,15 +98,8 @@ int main()
 {
 	enable_raw();
 	while(1) {
-		char c;
-		if(read(STDIN_FILENO, &c, 1) != 1)
-			continue;
-		if(iscntrl(c)) {
-			printf("%x\r\n", c);
-		} else {
-			printf("%x (%c)\r\n", c, c);
-		}
-		if(c == 'q') break;
+		editor_refresh_screen();
+		editor_process_key();
 	}
 	return 0;
 }
