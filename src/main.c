9bf21075c62164d2051dc054c09a5e1bb07942a4
Philip Simonson <psimonson1988@gmail.com>
2020-01-23 16:03:05 -0500

    Third part done, next making it a text viewer

--- src/main.c
+++ src/main.c
@@ -1,15 +1,32 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 #include <errno.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
+/* Editor version */
+#define PRSED_VERSION "1.0"
 /* Control+k macro */
 #define CTRL_KEY(k) ((k) & 0x1f)
+/* Editor special keys */
+enum {
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	DEL_KEY,
+	HOME_KEY,
+	END_KEY,
+	PAGE_UP,
+	PAGE_DOWN
+};
 /* Editor config structure */
 struct editor_config {
+	int cx, cy;
 	int screen_rows;
 	int screen_cols;
 	struct termios orig_termios;
@@ -52,17 +69,6 @@ void enable_raw()
 	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) < 0)
 		die("tcsetattr");
 }
-/* Read input from user.
- */
-char editor_read_key()
-{
-	int nread;
-	char c;
-	while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-		if(nread < 0 && errno != EAGAIN) die("read");
-	}
-	return c;
-}
 /* Gets the current position of the cursor.
  */
 int get_cursor_pos(int *rows, int *cols)
@@ -100,37 +106,185 @@ int get_window_size(int *rows, int *cols)
 		return 0;
 	}
 }
+/* Structure for append buffer. */
+struct abuf {
+	char *b;
+	int len;
+};
+/* Initial append buffer */
+#define ABUF_INIT	{NULL, 0}
+/* Create/Append to append buffer.
+ */
+void ab_append(struct abuf *ab, const char *s, int len)
+{
+	char *new = realloc(ab->b, ab->len+len);
+	if(new == NULL) return;
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len += len;
+}
+/* Free append buffer.
+ */
+void ab_free(struct abuf *ab)
+{
+	free(ab->b);
+}
 /* Draw rows for editor.
  */
-void editor_draw_rows()
+void editor_draw_rows(struct abuf *ab)
 {
 	int y;
 	for(y = 0; y < e.screen_rows; y++) {
-		write(STDOUT_FILENO, "~", 1);
+		if(y == e.screen_rows/3) {
+			char welcome[80];
+			int welcome_len;
+			int padding;
+			welcome_len = snprintf(welcome, sizeof(welcome),
+				"PRS Edit -- Version %s", PRSED_VERSION);
+			if(welcome_len > e.screen_cols)
+				welcome_len = e.screen_cols;
+			padding = (e.screen_cols-welcome_len)/2;
+			if(padding != 0) {
+				ab_append(ab, "~", 1);
+				padding--;
+			}
+			while(padding-- != 0) ab_append(ab, " ", 1);
+			ab_append(ab, welcome, welcome_len);
+		} else {
+			ab_append(ab, "~", 1);
+		}
 
+		ab_append(ab, "\x1b[K", 3);
 		if(y < e.screen_rows-1)
-			write(STDOUT_FILENO, "\r\n", 2);
+			ab_append(ab, "\r\n", 2);
 	}
 }
 /* Clear the screen.
  */
 void editor_refresh_screen()
 {
-	write(STDOUT_FILENO, "\x1b[2J", 4);
-	write(STDOUT_FILENO, "\x1b[H", 3);
-	editor_draw_rows();
-	write(STDOUT_FILENO, "\x1b[H", 3);
+	struct abuf ab = ABUF_INIT;
+	char buf[32];
+	ab_append(&ab, "\x1b[?25l", 6);
+	ab_append(&ab, "\x1b[H", 3);
+	editor_draw_rows(&ab);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", e.cy+1, e.cx+1);
+	ab_append(&ab, buf, strlen(buf));
+	ab_append(&ab, "\x1b[?25h", 6);
+	write(STDOUT_FILENO, ab.b, ab.len);
+	ab_free(&ab);
+}
+/* Read input from user.
+ */
+int editor_read_key()
+{
+	int nread;
+	char c;
+	while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if(nread < 0 && errno != EAGAIN) die("read");
+	}
+	if(c == '\x1b') {
+		char seq[3];
+
+		if(read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+		if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+		if(seq[0] == '[') {
+			if(seq[1] >= '0' && seq[1] <= '9') {
+				if(read(STDIN_FILENO, &seq[2], 1) != 1)
+					return '\x1b';
+				if(seq[2] == '~') {
+					switch(seq[1]) {
+					case '1': return HOME_KEY;
+					case '3': return DEL_KEY;
+					case '4': return END_KEY;
+					case '5': return PAGE_UP;
+					case '6': return PAGE_DOWN;
+					case '7': return HOME_KEY;
+					case '8': return END_KEY;
+					default: break;
+					}
+				}
+			} else {
+				switch(seq[1]) {
+				case 'A': return ARROW_UP;
+				case 'B': return ARROW_DOWN;
+				case 'C': return ARROW_RIGHT;
+				case 'D': return ARROW_LEFT;
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
+				default: break;
+				}
+			}
+		} else if(seq[0] == 'O') {
+			switch(seq[1]) {
+			case 'H': return HOME_KEY;
+			case 'F': return END_KEY;
+			default: break;
+			}
+		}
+		return '\x1b';
+	} else {
+		return c;
+	}
+}
+/* Move the cursor with keys 'a', 'd', 'w', 's'.
+ */
+void editor_move_cursor(int key)
+{
+	switch(key) {
+	case ARROW_LEFT:
+		if(e.cx != 0) {
+			e.cx--;
+		}
+	break;
+	case ARROW_RIGHT:
+		if(e.cx != e.screen_cols-1) {
+			e.cx++;
+		}
+	break;
+	case ARROW_UP:
+		if(e.cy != 0) {
+			e.cy--;
+		}
+	break;
+	case ARROW_DOWN:
+		if(e.cy != e.screen_rows-1) {
+			e.cy++;
+		}
+	break;
+	default:
+	break;
+	}
 }
 /* Process key presses from user.
  */
 void editor_process_key() {
-	char c = editor_read_key();
+	int c = editor_read_key();
 
 	switch(c) {
 	case CTRL_KEY('q'):
 		write(STDOUT_FILENO, "\x1b[2J", 4);
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
+	break;
+	case HOME_KEY:
+		e.cx = 0;
+	break;
+	case END_KEY:
+		e.cx = e.screen_cols-1;
+	break;
+	case PAGE_UP:
+	case PAGE_DOWN: {
+		int times = e.screen_rows;
+		while(times-- != 0)
+			editor_move_cursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+	} break;
+	case ARROW_UP:
+	case ARROW_DOWN:
+	case ARROW_LEFT:
+	case ARROW_RIGHT:
+		editor_move_cursor(c);
 	break;
 	default:
 	break;
@@ -140,6 +294,9 @@ void editor_process_key() {
  */
 void init_editor()
 {
+	e.cx = 0;
+	e.cy = 0;
+
 	if(get_window_size(&e.screen_rows, &e.screen_cols) < 0)
 		die("get_window_size");
 }
